292b7a529e188a0bb4725f94d7358456c82c7e0f
fabian <106248219+grassandtree@users.noreply.github.com>
2025-11-15 15:34:02 +0900

    try fixing deploy

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def get_user():
     return {'status': 'not_logged_in'}, 401
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
 
 if __name__ == "__main__":
     main()
